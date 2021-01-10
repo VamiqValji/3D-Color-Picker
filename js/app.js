4202041e746afa4ff30923f7d5e228e8f031b78e
Vamiq <vamiq.valji@gmail.com>
2021-01-09 20:40:54 -0500

    Cube rendered with light

--- js/app.js
+++ js/app.js
@@ -1,28 +1,59 @@
-const scene = new THREE.Scene();
+//  ALWAYS NEEDS SCENE, RENDERER, AND CAMERA
 
-const camera = new THREE.PerspectiveCamera(
-  90,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-// fov, aspect ratio, doesnt display if object is this close,
-// doesnt display is object is this near
-const renderer = new THREE.WebGLRenderer();
+let scene, camera, renderer, cube;
+// global vars
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+function init() {
+  scene = new THREE.Scene();
 
-document.body.appendChild(renderer.domElement);
+  camera = new THREE.PerspectiveCamera(
+    90,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  // fov, aspect ratio, doesnt display if object is this close,
+  // doesnt display is object is this near
+  renderer = new THREE.WebGLRenderer();
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.z = 5;
+  document.body.appendChild(renderer.domElement);
+
+  // cube
+  geometry = new THREE.BoxGeometry(2, 1, 1);
+  material = new THREE.MeshLambertMaterial({
+    color: 0x00ff00,
+    //   antialias: true,
+  });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  // light
+  const light = new THREE.AmbientLight(0x404040, 2); // soft white light
+  scene.add(light);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+  scene.add(directionalLight);
+
+  // camera
+  camera.position.z = 5;
+}
 
 function animate() {
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animate); // runs based on user's refresh rate
+  // Animations here
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
+
+const onWindowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}; // resizes 3d env to fit window when window is resized
+
+window.addEventListener("resize", onWindowResize, false);
+
+init();
 animate();
