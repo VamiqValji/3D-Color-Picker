a4874883faa4f5a4847b261161336a54d3a4ff01
Vamiq <vamiq.valji@gmail.com>
2021-01-09 22:35:12 -0500

    Basis
    I have the fundamentals (and functionality) of my idea in place

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //  ALWAYS NEEDS SCENE, RENDERER, AND CAMERA
 
-let scene, camera, renderer, cube;
+let scene, camera, renderer, cube, floor, ambientLight, pointLight;
 // global vars
 
 function init() {
@@ -20,6 +20,7 @@ function init() {
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x000000, 1); // background color
 
   document.body.appendChild(renderer.domElement);
 
@@ -29,36 +30,36 @@ function init() {
   geometry = new THREE.BoxGeometry(2, 1, 1);
   material = new THREE.MeshLambertMaterial({
     color: 0x00ff00,
-    //   antialias: true,
+    // antialias: true,
   });
   cube = new THREE.Mesh(geometry, material);
   cube.castShadow = true;
   cube.position.y = 0.5;
   scene.add(cube);
 
-  geometry = new THREE.BoxGeometry(4, 0.3, 4);
+  geometry = new THREE.BoxGeometry(20, 0.3, 20);
   material = new THREE.MeshLambertMaterial({
     color: 0xffffff,
     //   antialias: true,
   });
-  let floor = new THREE.Mesh(geometry, material);
+  floor = new THREE.Mesh(geometry, material);
   floor.receiveShadow = true;
   floor.position.y = -1;
   scene.add(floor);
 
   // LIGHTS
-  //   const light = new THREE.AmbientLight(0x404040, 2); // soft white light
-  //   scene.add(light);
+  ambientLight = new THREE.AmbientLight(0x404040, 3); // soft white light
+  scene.add(ambientLight);
   //   const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
   //   scene.add(directionalLight);
   //   directionalLight.rotation.z = 30;
-  const light = new THREE.PointLight(0xffffff, 1, 100);
-  light.position.set(1, 3, 0);
-  light.castShadow = true; // default false
-  scene.add(light);
+  pointLight = new THREE.PointLight(0xffffff, 1, 100);
+  pointLight.position.set(0, 3, 0);
+  pointLight.castShadow = true; // default false
+  scene.add(pointLight);
 
   // helper
-  const helper = new THREE.CameraHelper(light.shadow.camera);
+  const helper = new THREE.CameraHelper(pointLight.shadow.camera);
   scene.add(helper);
 
   // camera
@@ -70,6 +71,7 @@ function animate() {
   // Animations here
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
   //   camera.position.z -= 0.01;
   //   camera.rotation.y -= 0.005;
   renderer.render(scene, camera);
@@ -85,3 +87,8 @@ window.addEventListener("resize", onWindowResize, false);
 
 init();
 animate();
+
+const changeColor = () => {
+  console.log("test");
+  floor.material.color.setHex(0x4f0fff);
+};
